d8e8ea88456f5da7ecd224f484ebe3374ff8ef2f
Lucidreline <techservs@pm.me>
2020-10-30 11:54:48 -0700

    Removed what I don't need. Added a font and some starter code. Added ESLint.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Why Hello There</h1>
+    </div>
+  );
+}
+
+export default App;
